e5a83602d5f12be4bd5842e6d9d2fd47ce7e8289
Carlos Ponce <carlosponce156@gmail.com>
2020-07-22 09:48:03 -0500

    init test yonusa

---
+++ yonusa-test/controller/CollectionController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  yonusa-test
+//
+//  Created by Carlos Ponce on 22/07/20.
+//  Copyright © 2020 Carlos Ponce. All rights reserved.
+//
+
+import UIKit
+
+class CollectionController: UIViewController {
+
+    @IBOutlet weak var simpsonsCollection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
